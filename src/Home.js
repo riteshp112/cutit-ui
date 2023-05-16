34e7ba8ae7e01b834bf1aae207ffb697ceca2779
Ritesh <ritesh.patel@daffodilsw.com>
2023-05-16 17:38:05 +0530

    url

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ export const Home = () => {
       method: "post",
       body: JSON.stringify({ url: link }),
     })
-      .then((response) => {
+      .then(async (response) => {
         if (!response.ok) {
-          throw new Error(response.statusText);
+          throw new Error(await response.text());
         }
         return response.json();
       })
